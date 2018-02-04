35f0f9a500a430fd07f5464238c6168ce12df578
piot <piot@hotmail.com>
2018-02-04 16:54:32 +0100

    Able to set ip and port using environment variables

--- src/main.js
+++ src/main.js
@@ -9,13 +9,15 @@ let wss = new WebSocketServer({
 	port: 32000
 });
 
-const SERVER_IP = '127.0.0.1' // 'spelmotor.com'
-const SERVER_PORT = 32001
+const SERVER_IP = process.env.WS_UDP_TARGET_IP || '127.0.0.1'
+const SERVER_PORT = process.env.WS_UDP_TARGET_PORT || 32001
 
 var websocket_connection = null;
+console.log("WS-UDP proxy v0.0.1");
+console.log("> Target: " + SERVER_IP + ":" + SERVER_PORT);
 
 udpClient.on('message', (message, rinfo) => {
-	console.log("UDP Received:", message);
+	// console.log("UDP Received:", message);
 	try {
 		if (websocket_connection) {
 			websocket_connection.send(message);
@@ -31,7 +33,7 @@ wss.on('connection', (ws) => {
 	websocket_connection = ws;
 	websocket_connection.on('message', (message) => {
 		let msgBuff = new Buffer(message);
-		console.log("WS Received", msgBuff);
+		// console.log("WS Received", msgBuff);
 		udpClient.send(msgBuff, 0, msgBuff.length, SERVER_PORT, SERVER_IP);
 	});
 
