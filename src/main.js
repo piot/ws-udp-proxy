6273e0229fc4d98d9b268967c11b71f885903b25
piot <piot@hotmail.com>
2018-08-09 23:28:09 +0200

    feat: Able to run with unsecure websocket (ws://)

--- src/main.js
+++ src/main.js
@@ -1,43 +1,62 @@
-let Buffer = require('buffer').Buffer;
-let dgram = require('dgram');
-let WebSocketServer = require('ws').Server;
+const Buffer = require('buffer').Buffer;
+const dgram = require('dgram');
+const https = require('https');
+const ws = require('ws');
+const WebSocketServer = ws.Server;
+const fs = require('fs');
 
+function createWebsocketServer(unsecureFlag) {
+  var options;
 
-let udpClient = dgram.createSocket('udp4');
-const LISTEN_PORT = 32001
-let wss = new WebSocketServer({
-	port: LISTEN_PORT
-});
+  if (unsecureFlag) {
+    options = {port: LISTEN_PORT};
+  } else {
+    const httpsServer = https.createServer({
+      cert: fs.readFileSync('conf/certificate.pem'),
+      key: fs.readFileSync('conf/privkey.pem'),
+      port: LISTEN_PORT
+    });
+    httpsServer.listen(LISTEN_PORT);
+    options = {server: httpsServer};
+  }
+  const wss = new WebSocketServer(options);
 
+  return wss;
+}
+
+const LISTEN_PORT = 32001;
+const unsecureFlag = process.env.WS_UDP_UNSECURE;
 const SERVER_IP = process.env.WS_UDP_TARGET_IP || '127.0.0.1'
 const SERVER_PORT = process.env.WS_UDP_TARGET_PORT || 32002
 
+const wss = createWebsocketServer(unsecureFlag);
+const udpClient = dgram.createSocket('udp4');
 var websocket_connection = null;
-console.log("WS-UDP proxy v0.0.1");
-console.log("> Listen: " + LISTEN_PORT);
-console.log("> Target: " + SERVER_IP + ":" + SERVER_PORT);
 
+console.log('WS-UDP proxy v0.0.2');
+console.log('> Listen: ' + LISTEN_PORT);
+console.log('> Target: ' + SERVER_IP + ':' + SERVER_PORT);
+if (unsecureFlag) {
+  console.log('-- Warning! Unsecure!');
+}
 udpClient.on('message', (message, rinfo) => {
-	console.log("UDP Received:", message);
-	try {
-		if (websocket_connection) {
-			websocket_connection.send(message);
-		}
-	} catch (err) {
-		console.log(err);
-		websocket_connection = null;
-	}
+  // console.log("UDP Received:", message);
+  try {
+    if (websocket_connection) {
+      websocket_connection.send(message);
+    }
+  } catch (err) {
+    console.log(err);
+    websocket_connection = null;
+  }
 });
 
-
 wss.on('connection', (ws) => {
-	websocket_connection = ws;
-	websocket_connection.on('message', (message) => {
-		let msgBuff = new Buffer(message);
-		console.log("WS Received", msgBuff);
-		udpClient.send(msgBuff, 0, msgBuff.length, SERVER_PORT, SERVER_IP);
-	});
+  websocket_connection = ws;
+  websocket_connection.on('message', (message) => {
+    let msgBuff = new Buffer(message);
+    udpClient.send(msgBuff, 0, msgBuff.length, SERVER_PORT, SERVER_IP);
+  });
 
-	websocket_connection.on('error', (err) => {
-	});
+  websocket_connection.on('error', (err) => {});
 });
