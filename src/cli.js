0e90756f463c2ccc473171c3507c6eb3001478f0
piot <piot@hotmail.com>
2018-08-10 11:22:05 +0200

    feat: Converted to a command line application

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const ws_udp_proxy = require('./main.js');
+
+ws_udp_proxy.init();
--- src/main.js
+++ src/main.js
@@ -6,21 +6,22 @@ const WebSocketServer = ws.Server;
 const fs = require('fs');
 const chalk = require('chalk');
 const log = console.log;
+const pjson = require('./package.json');
 
-function createWebsocketServer(unsecureFlag) {
+function createWebsocketServer(unsecureFlag, listenPort) {
   var options;
 
   if (unsecureFlag) {
     options = {
-      port: LISTEN_PORT
+      port: listenPort
     };
   } else {
     const httpsServer = https.createServer({
       cert: fs.readFileSync('conf/certificate.pem'),
       key: fs.readFileSync('conf/privkey.pem'),
-      port: LISTEN_PORT
+      port: listenPort
     });
-    httpsServer.listen(LISTEN_PORT);
+    httpsServer.listen(listenPort);
     options = {
       server: httpsServer
     };
@@ -30,39 +31,44 @@ function createWebsocketServer(unsecureFlag) {
   return wss;
 }
 
-const LISTEN_PORT = 32001;
-const unsecureFlag = process.env.WS_UDP_UNSECURE;
-const SERVER_IP = process.env.WS_UDP_TARGET_IP || '127.0.0.1'
-const SERVER_PORT = process.env.WS_UDP_TARGET_PORT || 32002
+function initialize() {
+  const LISTEN_PORT = process.env.WS_UDP_SOURCE_PORT || 32001
+  const unsecureFlag = process.env.WS_UDP_UNSECURE;
+  const SERVER_IP = process.env.WS_UDP_TARGET_IP || '127.0.0.1'
+  const SERVER_PORT = process.env.WS_UDP_TARGET_PORT || 32002
+  const version = pjson.version;
 
-const wss = createWebsocketServer(unsecureFlag);
-const udpClient = dgram.createSocket('udp4');
-var websocket_connection = null;
+  const wss = createWebsocketServer(unsecureFlag, LISTEN_PORT);
+  const udpClient = dgram.createSocket('udp4');
+  var websocket_connection = null;
 
-log(chalk.magentaBright('WS-UDP proxy v0.0.2'));
-log('> Listen: ' + LISTEN_PORT);
-log('> Target: ' + SERVER_IP + ':' + SERVER_PORT);
-if (unsecureFlag) {
-  log(chalk.yellow('-- Warning! Unsecure!'));
-}
-udpClient.on('message', (message, rinfo) => {
-  // console.log("UDP Received:", message);
-  try {
-    if (websocket_connection) {
-      websocket_connection.send(message);
-    }
-  } catch (err) {
-    console.log(err);
-    websocket_connection = null;
+  log(chalk.magentaBright('WS-UDP proxy v' + version));
+  log('> Listen: ' + LISTEN_PORT);
+  log('> Target: ' + SERVER_IP + ':' + SERVER_PORT);
+  if (unsecureFlag) {
+    log(chalk.yellow('-- Warning! Unsecure!'));
   }
-});
-
-wss.on('connection', (ws) => {
-  websocket_connection = ws;
-  websocket_connection.on('message', (message) => {
-    let msgBuff = new Buffer(message);
-    udpClient.send(msgBuff, 0, msgBuff.length, SERVER_PORT, SERVER_IP);
+  udpClient.on('message', (message, rinfo) => {
+    // console.log("UDP Received:", message);
+    try {
+      if (websocket_connection) {
+        websocket_connection.send(message);
+      }
+    } catch (err) {
+      console.log(err);
+      websocket_connection = null;
+    }
   });
 
-  websocket_connection.on('error', (err) => {});
-});
+  wss.on('connection', (ws) => {
+    websocket_connection = ws;
+    websocket_connection.on('message', (message) => {
+      let msgBuff = new Buffer(message);
+      udpClient.send(msgBuff, 0, msgBuff.length, SERVER_PORT, SERVER_IP);
+    });
+
+    websocket_connection.on('error', (err) => {});
+  });
+}
+
+module.exports.init = initialize;
